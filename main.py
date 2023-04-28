872010a889495749a5dcfe758abc9bb0659ff92e
Kewin Caue <kewin.caue@hotmail.com>
2023-04-28 01:19:50 -0300

    teste commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Atividade 1
+"""INDICE = 13
+SOMA = 0
+K = 0
+
+if K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+print(SOMA);"""
+
+#Atividade 2
+"""lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+
+valor = int(input("Informe um número: "))
+
+if valor in lista:
+    print('O valor pertence a sequência!')
+else:
+    print('Esse valor não pertence a sequência')"""
+
+#Atividade 5
+"""frase = input('Digite uma frase: ')
+print(' Você digitou: {}'.format(frase))
+invertida = ' '.join(palavra[::-1] for palavra in frase.split())
+print('A frase que você digitou invertida fica: {}'.format(invertida))"""
